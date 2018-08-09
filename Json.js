9df43b1713b75bea9c058e3344913bbc1c3af5fd
KrisDuff456 <41322885+KrisDuff456@users.noreply.github.com>
2018-08-09 17:46:47 +0100

    Json gone weird
    no idea

--- Json.js
+++ Json.js
@@ -9,7 +9,41 @@ request.send();
 
 function loadJson(){
 	let requestData = request.response;
+
 	let heroHeading = document.createElement('h2');
 	    heroHeading.textContent = requestData['squadName'];
 		document.getElementsByTagName('body')[0].appendChild(heroHeading);
+		
+    let heroCity = document.createElement("p");
+	    heroCity.textContent = "Current Location: " + requestData['homeTown'] + " //  Created: " + requestData['formed'] + " //  Current Active Status: " + requestData['active'];
+		document.getElementsByTagName('body')[0].appendChild(heroCity);
 }
+function showHero(requestData){
+	
+	let myheroes = requestData['members'];
+		
+		for(let i = 0; i < myheroes.length;i++){
+			
+			let heroName = document.createElement('p');
+			let powerList = document.createElement('ul');
+			let myHero = document.createElement('article');
+			
+			heroName.textContent = myheroes[i].name;
+			heroName.textContent = "Secret Identity: " + myheroes[i].secretIdentity;
+			heroName.textContent = "Age: " + myheroes[i].age;
+			heroName.textContent = "Superpowers: ";
+			
+		    let superPowers = myheroes[i].powers;
+			for (var p = 0; p < superPowers.lenght; p++){
+				let listPower = document.createElement('li');
+				listPower.textContent = superPowers[p];
+				powerList.appendChild(listPower);
+		}
+		 myHero.appendChild(heroName);
+		 myHero.appendChild(powerList);
+		 
+		 section.appendChild(myHero);
+}
+
+}
+
